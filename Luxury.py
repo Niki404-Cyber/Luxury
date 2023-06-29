9da8f2c2531237380f1fd91570e24be8ad2b14cd
Mr. NIKI <93582194+Niki404-Cyber@users.noreply.github.com>
2023-06-29 18:54:15 +0300

    Update Luxury.py

--- Luxury.py
+++ Luxury.py
@@ -1,10 +1,6 @@
 import os, sys
 os.system('git pull')
-print('Enjoy Your Eid ')
-exit()
-'''import os, sys
-os.system('git pull')
 try:
     __import__("ELIF").menu()
 except Exception as e:
-    exit(str(e))'''
+    exit(str(e))

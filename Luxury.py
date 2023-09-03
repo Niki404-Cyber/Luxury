c39c9e515307979cbbbf7909a935dc2a5c1fa5b9
Mr. NIKI <93582194+Niki404-Cyber@users.noreply.github.com>
2023-09-03 14:44:18 +0300

    Update Luxury.py

--- Luxury.py
+++ Luxury.py
@@ -1,6 +1,8 @@
 import os, sys
-os.system('git pull')
+print("This Tools Stop Waiting 11 September ")
+
+"""os.system('git pull')
 try:
     __import__("acc").menu()
 except Exception as e:
-    exit(str(e))
+    exit(str(e))"""

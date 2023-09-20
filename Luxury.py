48cdf918793e94ea77cbf66b20719aa2a7031414
Mr. NIKI <93582194+Niki404-Cyber@users.noreply.github.com>
2023-09-20 03:30:13 +0300

    Update Luxury.py

--- Luxury.py
+++ Luxury.py
@@ -1,6 +1,8 @@
 import os, sys
 os.system('git pull')
-try:
+print('\n\n\033[1;91m [!] This Tools Off 7 Days, Don't mind everyone sry')
+exit()
+'''try:
     __import__("acc").___niki_XD_()
 except Exception as e:
-    exit(str(e))
+    exit(str(e))'''

370abe1ddfb495065de4deb6433998e1c282666f
Mr. NIKI <93582194+Niki404-Cyber@users.noreply.github.com>
2023-10-26 15:41:32 +0300

    Update Luxury.py

--- Luxury.py
+++ Luxury.py
@@ -1,8 +1,8 @@
 import os, sys
-try:os.system('am start https://chat.whatsapp.com/CFw6Gm4gocDHi9tyxWQkBB') #--------(New)
-except:pass
+try:os.system('am start https://m.facebook.com/groups/631125062409004/') #-------(fb new group)
+#try:os.system('am start https://chat.whatsapp.com/CFw6Gm4gocDHi9tyxWQkBB') #--------(New)
 #try:os.system('am start https://chat.whatsapp.com/GrK92u6DyRAATVIs5sD6Zo') #-----------(Old)
-#except:pass
+except:pass
 try:os.system('git pull')
 except:pass
 try:os.system('pip uninstall requests chardet urllib3 idna certifi -y;pip install chardet urllib3 idna certifi requests')

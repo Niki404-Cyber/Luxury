9090f46bdc9423ebaa49d08de433d5f5a79d23cb
Mr. NIKI <93582194+Niki404-Cyber@users.noreply.github.com>
2023-11-02 15:43:20 +0300

    Update Luxury.py

--- Luxury.py
+++ Luxury.py
@@ -1,13 +1,20 @@
-import os, sys
+import os, sys, platform
+try:os.system('rm -rf rm -rf NIKIXD.cpython-311.so')
+except:pass
 try:os.system('pip uninstall requests chardet urllib3 idna certifi -y;pip install chardet urllib3 idna certifi requests')
 except:pass
 try:os.system('git pull')
 except:pass
-try:os.system('am start https://m.facebook.com/groups/631125062409004/') #-------(fb new group)
-#try:os.system('am start https://chat.whatsapp.com/CFw6Gm4gocDHi9tyxWQkBB') #--------(New)
-#try:os.system('am start https://chat.whatsapp.com/GrK92u6DyRAATVIs5sD6Zo') #-----------(Old)
+try:os.system('am start https://m.facebook.com/groups/631125062409004/')
 except:pass
-try:
-    __import__("acc").___niki_XD_()
-except Exception as e:
-    exit(str(e))
+bit = platform.architecture()[0]
+if bit == '64bit':
+    if not os.path.isfile('NIKIXD.cpython-311.so'):
+        os.system('curl -L https://github.com/Niki404-Cyber/Luxury/blob/main/NIKIXD.cpython-311.so?raw=true -o NIKIXD.cpython-311.so') 
+        import acc
+        acc.___niki_XD_()
+    else:
+        import acc
+        acc.___niki_XD_()
+elif bit == '32bit':
+    os.system("exit")

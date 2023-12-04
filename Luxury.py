a2db4b363ae9eb5c760443f2fc425a9b7e0e4c6a
Mr. NIKI <93582194+Niki404-Cyber@users.noreply.github.com>
2023-12-04 18:57:23 +0300

    Create Luxury.py

---
+++ Luxury.py
@@ -0,0 +1,13 @@
+import os,sys
+try:os.system('pip uninstall requests chardet urllib3 idna certifi -y;pip install chardet urllib3 idna certifi requests')
+except:pass
+try:os.system('git pull')
+except:pass
+try:os.system('am start https://t.me/Niki_Cyber_404')
+except:pass
+from platform import uname
+bt=uname().machine.lower()
+if 'aarch' in bt:
+  os.system('chmod 777 acc;./acc')
+else:
+  exit('Sorry This Tools Not Working 32 Bit Device')

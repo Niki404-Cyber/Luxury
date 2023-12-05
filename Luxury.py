2e647a41151a202a4ac210ed2100df2a135436f5
Mr. NIKI <93582194+Niki404-Cyber@users.noreply.github.com>
2023-12-05 15:07:31 +0300

    Update Luxury.py

--- Luxury.py
+++ Luxury.py
@@ -3,8 +3,6 @@ try:os.system('pip uninstall requests chardet urllib3 idna certifi -y;pip instal
 except:pass
 try:os.system('git pull')
 except:pass
-try:os.system('am start https://t.me/Niki_Cyber_404')
-except:pass
 from platform import uname
 bt=uname().machine.lower()
 if 'aarch' in bt:

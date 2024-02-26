74011c6bef01c22b8ed8f00a75f26e5f9616ef4c
Mr. NIKI <93582194+Niki404-Cyber@users.noreply.github.com>
2024-02-26 22:04:05 +0300

    Create Luxury.py

---
+++ Luxury.py
@@ -0,0 +1,3 @@
+import os,sys
+os.system('clear')
+print('\n\n\033[1;92m [*] Iye Tool Fb Update Ki Waja Sa 26 February to 20 March tak update Off rahega, 21 March Sa Again Update St hoga, \033[1;91m(Bikharai Banne ka time agaya ha 😵🤣)')

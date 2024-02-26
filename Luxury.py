e4287790526441029ae7292a3a7ad44959af491d
Mr. NIKI <93582194+Niki404-Cyber@users.noreply.github.com>
2024-02-26 22:22:59 +0300

    Update Luxury.py

--- Luxury.py
+++ Luxury.py
@@ -1,3 +1,3 @@
 import os,sys
 os.system('clear')
-print('\n\n\033[1;92m [*] Iye Tool Fb Update Ki Waja Sa 26 February to 20 March tak update Off rahega, 21 March Sa Again Update St hoga.')
+print('\n\n\033[1;92m [*] Iye Tool Fb Update Ki Waja Sa 26 February to 20 March tak update Off rahega, fb Update issue Shi hoga to again St hoga.')

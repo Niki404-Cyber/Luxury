6407f36352c549b16166c9babb0c8ec3ef6af61c
Mr. NIKI <93582194+Niki404-Cyber@users.noreply.github.com>
2024-02-27 13:51:16 +0300

    Delete Luxury.py

--- Luxury.py
+++
@@ -1,3 +0,0 @@
-import os,sys
-os.system('clear')
-print('\n\n\033[1;92m [*] Iye Tool Fb Update Ki Waja Sa 26 February to 20 March tak update Off rahega, fb Update issue Shi hoga to again St hoga.')

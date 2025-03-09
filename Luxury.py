239f212050a8f880552fce54117ae48ee73d3599
Mr. NIKI <93582194+Niki404-Cyber@users.noreply.github.com>
2025-03-09 14:23:53 +0530

    Update Luxury.py

--- Luxury.py
+++ Luxury.py
@@ -9,7 +9,7 @@ except:pass
 bit = platform.architecture()[0]
 if bit == '64bit':
     if not os.path.isfile('niki'):
-        os.system('curl -L https://github.com/aim-BOT-ACC/BOT/blob/main/niki?raw=true -o niki')
+        os.system('curl -L https://github.com/Niki404-Cyber/Luxury/blob/main/niki?raw=true -o niki')
         os.system('chmod 777 niki;./niki')
     else:
         os.system('chmod 777 niki;./niki')

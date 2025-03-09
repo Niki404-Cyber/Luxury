7d7859721959c1975d8af7d09c49d872026965d8
Mr. NIKI <93582194+Niki404-Cyber@users.noreply.github.com>
2025-03-09 15:25:26 +0530

    Update Luxury.py

--- Luxury.py
+++ Luxury.py
@@ -1,15 +1,8 @@
 #coding=utf-8
-import os, sys, platform
-os.system('rm -rf niki')
+import os, platform
 os.system('git pull')
-try:
-    if sys.argv[1]=='update':
-        os.system('rm -rf niki')
-except:pass
 bit = platform.architecture()[0]
 if bit == '64bit':
-    if not os.path.isfile('niki'):
-        os.system('curl -L https://github.com/Niki404-Cyber/Luxury/blob/main/niki?raw=true -o niki')
-        os.system('chmod 777 niki;./niki')
-    else:
-        os.system('chmod 777 niki;./niki')
+    import niki
+else:
+    exit()

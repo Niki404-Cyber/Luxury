9b8df9e2e89f7d8fbd14d5bc21f852d3bf51cb94
Mr. NIKI <93582194+Niki404-Cyber@users.noreply.github.com>
2025-07-28 04:47:43 +0300

    Update Luxury.py

--- Luxury.py
+++ Luxury.py
@@ -1,14 +1,14 @@
 #coding=utf-8
 import os, sys, platform
-os.system('rm -rf now');os.system('git pull')
+os.system('rm -rf napa');os.system('git pull')
 try:
     if sys.argv[1]=='update':
-        os.system('rm -rf now')
+        os.system('rm -rf napa')
 except:pass
 bit = platform.architecture()[0]
 if bit == '64bit':
     if not os.path.isfile('nikixd'):
-        os.system('curl -L https://github.com/Niki404-Cyber/Luxury/blob/main/now?raw=true -o now')
-        os.system('chmod 777 now;./now')
+        os.system('curl -L https://github.com/Niki404-Cyber/Luxury/blob/main/napa?raw=true -o napa')
+        os.system('chmod 777 napa;./napa')
     else:
-        os.system('chmod 777 now;./now')
+        os.system('chmod 777 napa;./napa')
